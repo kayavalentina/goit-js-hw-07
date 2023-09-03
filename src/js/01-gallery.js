00ca5072001ab9a3e8a8193a40d6f0564307e325
kayavalentina <kayavalentina1993@gmail.com>
2023-09-03 16:33:58 +0200

    finish all

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,36 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const container = document.querySelector('.gallery');
- 
+
 const markup = createMarkup(galleryItems);
-
 container.insertAdjacentHTML('beforeend', markup);
-container.addEventListener('click', handlerProductClick);
+container.addEventListener('click', openModal);
 
-function createMarkup(arr){
-    return arr.map(({ preview, original, description }) => {
-        return `<li class="gallery__item"> 
+let modalInstance = null; 
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item"> 
         <a class="gallery__link" href="${original}">
           <img class="gallery__image" 
           src="${preview}" data-source="${original}" 
           alt="${description}"/>
         </a> 
-        </li>`
-    }).join('');
+        </li>`;
+    })
+    .join('');
 }
-  
-function handlerProductClick(event) {
-     if(event.target === event.currentTarget){
-        return;
-     }
-    event.preventDefault();
-     
-    const instance = basicLightbox.create(`
+
+function openModal(event) {
+  event.preventDefault();
+
+  const clickedElement = event.target.closest('.gallery__item');
+  if (!clickedElement) return;
+
+  modalInstance = basicLightbox.create(`
     <div class="modal">
-     <img src="${event.target.dataset.source}" alt="${event.target.alt}" />
+      <img src="${clickedElement.querySelector('.gallery__image').dataset.source}" alt="${clickedElement.querySelector('.gallery__image').alt}" />
     </div>
-    `)
-    instance.show();
+  `);
+
+  modalInstance.show();
+
+  window.addEventListener('keydown', closeModalOnEscape);
+}
+
+function closeModal() {
+  if (modalInstance) {
+    modalInstance.close();
+    modalInstance = null;
+    
+    window.removeEventListener('keydown', closeModalOnEscape);
+  }
+}
+
+function closeModalOnEscape(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
 }
 
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector('.gallery');
 
+const markup = createMarkup(galleryItems);
+container.insertAdjacentHTML('beforeend', markup);
+container.addEventListener('click', openModal);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250, 
+  });
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item"> 
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" 
+          src="${preview}"  
+          alt="${description}"/>
+        </a> 
+        </li>`;
+    })
+    .join('');
+}
+function openModal(event) {
+    event.preventDefault();
+}
+    
 console.log(galleryItems);
